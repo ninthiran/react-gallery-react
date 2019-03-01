ea1fc1795f4d55e2d974c81d11eadd1d36c10535
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 13:57:09 +0000

    changeing layout structures of AlbumList rendering way

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AlbumList from "./components/AlbumList";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <AlbumList />
+      </div>
+    );
   }
 }
 
--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import api from "../Api/api";
-import GridBox from "./GribBox";
+// import GridBox from "./GribBox";
 
 class AlbumList extends Component {
   state = {
@@ -11,38 +11,42 @@ class AlbumList extends Component {
   componentDidMount = () => {
     api.getDataList().then(res => {
       const data = res.data;
-      let uniqueAlbumList = [];
       const getUniqueAlbumIds = [...new Set(data.map(item => item.albumId))];
-      getUniqueAlbumIds.forEach(unique => {
-        uniqueAlbumList[unique - 1] = data
-          .map(x => {
-            return x;
-          })
-          .filter(x => x.albumId == unique)[0];
-      });
-      this.setState({ uniqueAlbumList });
+      // getUniqueAlbumIds.forEach(unique => {
+      //   uniqueAlbumList[unique - 1] = data
+      //     .map(x => {
+      //       return x;
+      //     })
+      //     .filter(x => x.albumId == unique)[0];
+      // });
+      this.setState({ uniqueAlbumList: getUniqueAlbumIds });
     });
   };
 
   renderAlbumList = (x, y) => {
     return (
-      <GridBox
-        albumId={x.albumId}
-        key={y}
-        thumbnailUrl={x.thumbnailUrl}
-        title={x.title}
-        label={x.title}
-        linkEnable={true}
-      />
+      <li key={y}>
+        <span key={y} id={x} onClick={this.albumHandler} id={x}>
+          Album - {x}
+        </span>
+      </li>
     );
   };
 
   render() {
     return (
-      <div className="container">
-        <h1>Select an album to view image </h1>
-        <div className="row">
-          {this.state.uniqueAlbumList.map((x, y) => this.renderAlbumList(x, y))}
+      <div className="wrapper">
+        <div className="scrollable">
+          <nav
+            id="sidebar"
+            className="navbar navbar-expand-lg navbar-light bg-light"
+          >
+            <ul className="list-unstyled components">
+              {this.state.uniqueAlbumList.map((x, y) =>
+                this.renderAlbumList(x, y)
+              )}
+            </ul>
+          </nav>
         </div>
       </div>
     );
