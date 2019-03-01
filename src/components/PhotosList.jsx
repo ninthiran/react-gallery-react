851870d63adc6d1c0bb4f1feee0d9abeb10a0fc3
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 14:05:48 +0000

    modified layout method

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -5,41 +5,30 @@ import GridBox from "./GribBox";
 class PhotosList extends Component {
   constructor(props) {
     super(props);
-    const {
-      match: { params }
-    } = this.props;
     this.state = {
-      AlbumID: params.id,
-      photoData: []
+      largeTest: []
     };
   }
 
-  componentDidMount = () => {
-    api.getDataList(`?albumId=${this.state.AlbumID}`).then(list => {
-      console.log(1);
-      this.setState({ photoData: list.data });
-    });
-  };
-
   imageGridGenerator = x => {
     return (
-      <GridBox
-        label={x.title}
-        key={x.id}
-        thumbnailUrl={x.thumbnailUrl}
-        title={x.title}
-        linkEnable={false}
-        url={x.url}
-      />
+      <div key={x.id} className="col-lg-2 col-md-4 col-sm-4 col-xs-6 grid-mar">
+        <img src={x.thumbnailUrl} id={x.id} alt="" />
+        <span>{x.title}</span>
+      </div>
     );
   };
 
   render() {
     return (
-      <div className="container">
-        <h1>PhotosList</h1>
+      <div id="content">
+        <h1>
+          {this.props.photoData.length === 0
+            ? "Select Album form the side bar to view photos"
+            : ""}
+        </h1>
         <div className="row">
-          {this.state.photoData.map(x => this.imageGridGenerator(x))}
+          {this.props.photoData.map(x => this.imageGridGenerator(x))}
         </div>
       </div>
     );
