3c5b20da5c01e11a7097164b974bfb0138324e70
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-01 14:45:12 +0000

    added onClick event into the photolist components

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
-import api from "../Api/api";
 import GridBox from "./GribBox";
 
 class PhotosList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      largeTest: []
+      largeTest: [],
+      showPopUp: false
     };
   }
 
   imageGridGenerator = x => {
     return (
       <div key={x.id} className="col-lg-2 col-md-4 col-sm-4 col-xs-6 grid-mar">
-        <img src={x.thumbnailUrl} id={x.id} alt="" />
+        <img
+          src={x.thumbnailUrl}
+          id={x.id}
+          alt=""
+          onClick={this.imagePopupLoader}
+        />
         <span>{x.title}</span>
       </div>
     );
+  };
+
+  imagePopupLoader = e => {
+    const id = e.target.id;
+    const url = this.props.photoData.filter(x => x.id == id);
+    this.setState({ largeTest: url[0] }, function() {
+      console.log(this.state.largeTest);
+    });
+    this.popupToggle();
+  };
+
+  popupToggle = () => {
+    this.setState({
+      showPopUp: !this.state.showPopUp
+    });
   };
 
   render() {
